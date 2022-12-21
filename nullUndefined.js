51730abb246789ec43a27e4210cd4ee0f36bbdd6
unknown <lukacc008@gmail.com>
2022-12-21 02:00:58 +0100

    Differences between null and undefined

---
+++ nullUndefined.js
@@ -0,0 +1,21 @@
+// What will be logged in first example and what in the second one?
+
+let var1;
+console.log(var1);
+console.log(typeof var1);
+
+let var2 = null;
+console.log(var2);
+console.log(typeof var2);
+
+
+
+
+let a = null;
+let b = undefined;
+
+console.log(a == b)
+// TRUE
+
+console.log(a === b)
+// FALSE
