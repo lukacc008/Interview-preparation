431734e70236d00b20bd89aeff6c3d014664d18f
unknown <lukacc008@gmail.com>
2022-12-23 16:13:19 +0100

    learning

---
+++ Interview-exercises/reverseWords.js
@@ -0,0 +1,5 @@
+// Reverse each word in sentence
+
+const str = "Welcome to this Javascript Guide";
+const reverseString = str => str.split(" ").reverse().join(" ")
+console.log(reverseString(str));
